7f2d91fdbcce95050f0676d2fb6f7ea55398e25e
khairulimran-99 <khaorul991018@gmail.com>
2021-07-01 15:45:32 +0000

    Commit Client Receipt

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import signal
 import sys
 
 socket = socket.socket()
-host = '192.168.114.6'
+host = '192.168.56.103'
 port = 8888
 
 print('Waiting for connection...')
@@ -30,7 +30,7 @@ print("                         [D]  Hoodie            |")
 print("         | [E]  Seluar Jeans", end =" ")
 print("                   [F]  Seluar Slack      |")
 print("         | [G]  Seluar Slack", end =" ")
-print("                   [H]  Topi              |")
+print("                                          |")
 print("   --------------------------------------------------------------------------")
 print("============================================================================")
 
@@ -38,7 +38,7 @@ print("=========================================================================
 
 while True:
     opt = input('\nSelect Your Option [Code Bundle] Press "X" if you are done..\n> ')
-    if opt == "A" or opt == "B" or opt == "C" or opt == "D" or opt == "E" or opt == "F" or opt == "G" or opt == "H":
+    if opt == "A" or opt == "B" or opt == "C" or opt == "D" or opt == "E" or opt == "F" or opt == "G":
         qty = input("\nQuantity per Order: ")
         prc = '0.00'
         total = 0.00
@@ -46,30 +46,32 @@ while True:
         socket.send(str.encode(keServer))
         price = socket.recv(1024)
         price = float(price)
+        prod = socket.recv(1024)
+        prod = str(prod)
 
-        size = input ('\nWe have S/M/L/XL/2XL/3XL size. If size XL-->3XL, add RM10.00 \nChoose your size: ')
+        size = input ('We have S/M/L/XL/2XL/3XL size. If size XL-->3XL, add RM10.00 \nChoose your size: ')
         if size == "S":
-           print("Size: S\n")
+           saiz = "S\n"
            total = price
 
         elif size == "M":
-           print("Size: M\n")
+           saiz = "M\n"
            total = price
 
         elif size == "L":
-           print("Size: L\n")
+           saiz = "L\n"
            total = price
 
         elif size == "XL":
-           print("Size: XL\n")
+           saiz = "XL [+RM10.00]\n"
            total = price + 10.00
 
         elif size == "2XL":
-           print("Size: 2XL\n")
+           saiz = "2XL [+RM10.00]\n"
            total = price + 10.00
 
         elif size == "3XL":
-           print("Size: 3XL\n")
+           saiz = "3XL [+10.00]\n"
            total = price + 10.00
 
 
@@ -77,60 +79,68 @@ while True:
            print("WRONG INPUT SIZE!!!\n")
            break
 
-        add = input('\nAdd Onn (Y/N): ')
+        add = input('\nAdd Onn [+RM 10.00] (Y/N): ')
         if add == "Y":
          addon = input('We have [A]Face Mask | [B]Lanyard | [C]Mafla\nChoose your Add on (A/B/C): ')
          if addon == "A":
-          print("Face Mask[+RM15.00]\n")
-          total +=  15.00
+          add = "Face Mask [+RM10.00]\n"
+          total +=  10.00
 
          elif addon == "B":
-          print("Lanyard[+RM10.00]\n")
+          add = "Lanyard [+10.00]\n"
           total += 10.00
 
          elif addon == "C":
-          print("Mafla[+RM20.00]\n")
-          total += 20.00
+          add = "Mafla [+10.00]\n"
+          total += 10.00
 
          else:
-          print("WRONG ADD ONN!!!")
+          print( "WRONG ADD ONN!!!")
           break
 
         else:
-         print("No Add On\n")
+         add = "No Add On\n"
 
 
         member = input('\nMembership (Y/N)?: ')
         if member == "Y":
-            print("Member\n")
+            ahli = "Member [-30%]\n"
             tot = total - (total*0.3)
 
 
         elif member == "N":
-            print("Non-Member\n")
+            ahli = "Non-Member\n"
             tot = total
 
         else:
-            print("Non-Member\n")
+            ahli = "Non-Member\n"
             tot = total
 
 
-        delivery = input('\nDeliver Option - [A]COD [B]POSTAGE[+RM 5.00]: ')
+        delivery = input('\nDeliver Option - [A]COD  [B]POSTAGE[+RM 5.00]: ')
         if delivery == "A":
-           print("COD\n")
-           sum = tot
-           print("Total to Pay: RM " + str(sum))
+           hantar = "COD\n"
+           print("Total to Pay: RM " + str(tot))
 
         elif delivery == "B":
-           print("Postage\n")
-           sum = tot + 5.00
-           print("Total to Pay: RM " + str(sum))
+           hantar = "Postage [RM5.00]\n"
+           tot += 5.00
+           print("Total to Pay: RM " + str(tot))
 
         else:
-           print("Postage\n")
-           sum = tot + 5.00
-           print("Total to Pay: RM " + str(sum))
+           hantar = "Postage [RM5.00]\n"
+           tot += 5.00
+           print("Total to Pay: RM " + str(tot))
 
+        
+        print("\n\t\t\t*****RECEIPT*****\n")
+        print("\t\t\tITEM: " + prod)
+        print("\t\t\tSIZE: " + saiz)
+        print("\t\t\tADD-ON: " + add)
+        print("\t\t\tMEMBERSHIP: " + ahli)
+        print("\t\t\tDELIVERY: " + hantar)
+        print("\t\t\tTotal to Pay: RM " + str(tot) + "\n")
+        print("\t\t\t*****THANK YOU*****\n")
 
     elif opt == 'X':
         print('YOUR ORDER HAS BEEN SUCCESFULLY RECORDED..\nTHANK YOU FOR YOUR ORDER :)')
