41440230261c0daee016aa93b6731e50076fe2f8
Amirul Ikmal <amirul.ikmal88@gmail.com>
2021-07-02 17:07:32 +0000

    client

--- client.py
+++ client.py
@@ -14,23 +14,36 @@ try:
 except socket.error as e:
         print(str(e))
 
+try:
+        file = open(r'bundle.txt', 'r')
+
+except :
+        file = open(r'bundle.txt', 'w')
+        file.write('name, prod, saiz, add, ahli, hantar, address, total \n')
+        file.close()
+        file = open(r'bundle.txt', 'r')
+
+file = open(r'bundle.txt', 'a')
+
 
 Response = socket.recv(2048)
 print(Response.decode("utf-8"))
+
+
 
 print ( "\n============================================================================\n"
                "      * *** *** ***WELCOME TO AIM'S BUNDLE ONLINE SHOP *** **** *** *\t\t\t\n"
                         "============================================================================\n");
 
 print("   --------------------------------------------------------------------------")
-print("         | [A]  Kemeja" , end =" ")
-print("                         [B]  Tshirt            |")
-print("         | [C]  Jacket", end =" ")
-print("                         [D]  Hoodie            |")
-print("         | [E]  Seluar Jeans", end =" ")
-print("                   [F]  Seluar Slack      |")
-print("         | [G]  Seluar Slack", end =" ")
-print("                                          |")
+print("    | [A]  Kemeja - RM 40" , end =" ")
+print("                     [B]  Tshirt - RM 30          |")
+print("    | [C]  Jacket - RM 90", end =" ")
+print("                     [D]  Hoodie - RM 70          |")
+print("    | [E]  Seluar Jeans - RM 45", end =" ")
+print("               [F]  Seluar Slack - RM 45    |")
+print("    | [G]  Seluar Track - RM 25", end =" ")
+print("                                       ")
 print("   --------------------------------------------------------------------------")
 print("============================================================================")
 
@@ -38,7 +51,7 @@ print("=========================================================================
 
 while True:
     opt = input('\nSelect Your Option [Code Bundle] Press "X" if you are done..\n> ')
-    if opt == "A" or opt == "B" or opt == "C" or opt == "D" or opt == "E" or opt == "F" or opt == "G":
+    if opt == "A" or opt == "a" or opt == "B" or opt == "b" or opt == "C" or opt == "c" or opt == "D" or opt == "d" or opt == "E" or opt == "e" or opt == "F" or opt == "f" or opt == "G" or opt == "g":
         qty = input("\nQuantity per Order: ")
         prc = '0.00'
         total = 0.00
@@ -121,12 +134,14 @@ while True:
         delivery = input('\nDeliver Option - [A]COD  [B]POSTAGE[+RM 5.00]: ')
         if delivery == "A":
            hantar = "COD\n"
+           name = input('\nEnter your name: ')
            address = input('\nEnter your home address: ')
            print("Total to Pay: RM " + str(tot))
 
         elif delivery == "B":
            hantar = "Postage [RM5.00]\n"
            tot += 5.00
+           name = input('\nEnter your name: ')
            address = input('\nEnter your home address: ')
            print("Total to Pay: RM " + str(tot))
            print("\nOur Account Number: AIM Bundle - 1234567891011 (Bank Islam) \nPlease send the transaction receipt to us after transfer the money \nThank You ^^")
@@ -134,20 +149,25 @@ while True:
         else:
            hantar = "Postage [RM5.00]\n"
            tot += 5.00
+           name = input('\nEnter your name: ')
            address = input('\nEnter your home address: ')
            print("Total to Pay: RM " + str(tot))
            print("\nOur Account Number: AIM Bundle - 1234567891011 (Bank Islam) \nPlease send the transaction receipt to us after transfer the money \nThank You ^^")
 
 
-        print("\n\t\t\t*******RECEIPT*******\n")
+        print("\nThis is your receipt. Please save it\n")
+        print("\n\t\t\t*********RECEIPT*********\n")
         print("\t\t\t AIMS'S ONLINE BUNDLE")
-        print("\t\t\tITEM: " + prod)
+        print("\t\t\tNAME: " + name)
+        print("\n\t\t\tITEM: " + prod)
         print("\t\t\tSIZE: " + saiz)
         print("\t\t\tADD-ON: " + add)
         print("\t\t\tMEMBERSHIP: " + ahli)
         print("\t\t\tDELIVERY: " + hantar)
+        print("\t\t\tHOME ADDRESS: " + address)
         print("\t\t\tTotal to Pay: RM " + str(tot) + "\n")
-        print("\n\t\t\t*******THANK YOU*******\n")
+        print("\t\t\tOur Account Number: AIM Bundle - 1234567891011 (Bank Islam) \n\t\t\tPlease send the transaction receipt to us after transfer the money")
+        print("\n\t\t\t*********THANK YOU*********\n")
 
     elif opt == 'X':
         print('YOUR ORDER HAS BEEN SUCCESFULLY RECORDED..\nTHANK YOU FOR YOUR ORDER :)')
@@ -156,5 +176,9 @@ while True:
     else:
         print("WRONG INPUT, TRY AGAIN!!")
         break
+
+write = name + ' ,' + str(prod) + ' ,' + str(saiz) + ' ,' + str(add) + str(ahli) + ' ,' + str(hantar) + ' ,' + str(address) + ' ,' + str(total) + '\n'
+file.write(write)
+file.close
 
 socket.close()
