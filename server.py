c87c20851eea895057fa73d58d7996d3e9b19627
khairulimran-99 <khaorul991018@gmail.com>
2021-07-04 16:59:43 +0000

    Commit Server

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from multiprocessing import Process
 
 def process_start(s_sock):
 
-    s_sock.send(str.encode('\n\t\t*#*#*AIMS BUNDLE COLLECTION*#*#*\t\t\t'))
+    s_sock.send(str.encode('\n\t\t*#*#*CONNECTED*#*#*\t\t\t'))
     while True:
         data = s_sock.recv(2048)
         data = data.decode("utf-8")
@@ -20,37 +20,37 @@ def process_start(s_sock):
             qty = int(num)
             prc = float(value)
 
-            if opt[0]  == 'A' or 'a':
+            if opt[0]  == 'A':
                 opt = 'Kemeja'
                 prc = 40
                 ans = qty * (prc)
 
-            elif opt[0] == 'B' or 'b':
+            elif opt[0] == 'B':
                 opt = 'Tshirt'
                 prc = 30
                 ans = qty * (prc)
 
-            elif opt[0] == 'C' or 'c':
+            elif opt[0] == 'C':
                 opt = 'Jacket'
                 prc = 90
                 ans = qty * (prc)
 
-            elif opt[0] == 'D' or 'd':
+            elif opt[0] == 'D':
                 opt = 'Hoodie'
                 prc = 70
                 ans = qty * (prc)
 
-            elif opt[0] == 'E' or 'e':
+            elif opt[0] == 'E':
                 opt = 'Seluar Jeans'
                 prc = 45
                 ans = qty * (prc)
 
-            elif opt[0] == 'F' or 'f':
+            elif opt[0] == 'F':
                 opt = 'Seluar Slack'
                 prc = 45
                 ans = qty * (prc)
 
-            elif opt[0] == 'G' or 'g':
+            elif opt[0] == 'G':
                 opt = 'Seluar Track'
                 prc = 25
                 ans = qty * (prc)
@@ -62,11 +62,11 @@ def process_start(s_sock):
             sendClient = ans
             keClient = str(opt)
             print(calculate)
-            print ('ORDER RECEIVED!!')
+            print ('ORDER RECEIVED!! \n')
             #break
 
         except:
-            print ('Connection Terminated')
+            print ('Connection Terminated \n')
             sendToClient = ('Connection Terminated')
             break
 
